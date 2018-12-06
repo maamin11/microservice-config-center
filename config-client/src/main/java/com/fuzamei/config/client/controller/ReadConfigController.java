51a7ca64fa8fb7079cd19dd40d32cbd96951bcf4
maamin1 <mam@disanbo.com>
2018-12-06 11:35:52 +0800

    delete

---
+++ config-client/src/main/java/com/fuzamei/config/client/controller/ReadConfigController.java
@@ -0,0 +1,10 @@
+package com.fuzamei.config.client.controller;
+
+/**
+ * @program: microservice-config-center
+ * @description:
+ * @author: Andy
+ * @create: 2018-12-06 10:07
+ **/
+public class ReadConfigController {
+}
